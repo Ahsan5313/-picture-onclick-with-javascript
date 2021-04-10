08568ebb3eebab9e33ca2c2eb8cd2ef2739a115e
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-10 12:02:50 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,6 @@
+function picture_img(){
+    document.querySelector("img").src="124845955_1103265426800256_1793868418948173683_o.jpg"
+}
+function picture_img1(){
+    document.querySelector("img").src="forcast.png"
+}
